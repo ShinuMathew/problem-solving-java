b3690b23c00c6f490177c2352fd07d0b2c505b5e
Shinu Mathew <shinu.mathew@samsung.com>
2021-10-21 22:33:37 +0530

    Added Continous Maximu product

--- src/com/problem_solving/interview_questions/ContiguousNonEmptySubarray.java
+++ src/com/problem_solving/interview_questions/ContiguousNonEmptySubarray.java
@@ -9,20 +9,34 @@ public class ContiguousNonEmptySubarray {
 	
 	public static void main(String[] args) {
 				
-		int[] nums = {2, 3, -2, 4};
-		getproduct(nums);		
+		int[] nums = {2, 3, 2, -4};
+		System.out.println("Maximum product subarray is : "+getproduct(nums));	
 		
 	}
 	
-	private static void getproduct(int[] nums) {
+	private static int getproduct(int[] nums) {
+		int lastProduct = 0 , currentproduct = 0, maxProduct = 0;
+		
 		for(int i = 0; i<nums.length-1; i++) {
 			for(int j = i+1; j<nums.length; j++) {
-				System.out.println(i+", "+j);
-				
+				currentproduct = getProductOfArray(nums, i, j);				
+				if(lastProduct == 0)
+					lastProduct = currentproduct;				
+				else {
+					maxProduct = lastProduct > currentproduct ? lastProduct : currentproduct;
+					lastProduct = maxProduct;
+				}												
 			}
 		}
+		return maxProduct;
 	}
 	
-	
+	private static int getProductOfArray(int[] nums, int startIndex, int endIndex) {		
+		int product = 0;
+		for(int i = startIndex; i<=endIndex; i++) {
+			product = (product == 0) ? nums[i] : product * nums[i];			
+		}
+		return product;
+	}
 
 }
